17ec251209d1383a4a4f59943cdb68d3d3778f41
avamys106 <avamys106@naver.com>
2024-01-19 15:47:58 +0900

    2024-01-19 프로젝트 2일차

--- src/banking1/Account.java
+++ src/banking1/Account.java
@@ -1,123 +1,21 @@
 package banking1;
 
-import java.util.Scanner;
-
-class Account {
-	Account[] myAccounts = new Account[50];
-	int numOfAccounts;
-	
+public class Account {
 	String acnumber;
 	String name;
 	int mymoney;
+	public Account() {
+	}
 	
 	public Account(String acnumber, String name, int mymoney) {
 		this.acnumber = acnumber;
 		this.name = name;
 		this.mymoney = mymoney;
 	}
-	public Account() {
-	}
-	
-	public void menuShow() {
-		System.out.println("----------Menu---------");
-		System.out.print("1.계좌 개설 ");
-		System.out.print("2.입 금 ");
-		System.out.println("3.출 금 ");
-		System.out.print("4.계좌정보출력 ");
-		System.out.print("5.프로그램종료 ");
-	}
-	
-	
-//	public void makeAccount() {
-//		Scanner scanner = new Scanner(System.in);
-//		String iAcnumber, iName;
-//		int iMoney;
-//		System.out.println("***신규계좌개설***");
-//		System.out.print("계좌번호:");iAcnumber = scanner.nextLine();
-//		System.out.print("고객이름:");iName = scanner.nextLine();
-//		System.out.print("잔고:");iMoney = scanner.nextInt();
-//		System.out.println("계좌개설이 완료되었습니다.");
-//		Account make = new Account(iAcnumber, iName, iMoney);
-//		myAccounts[numOfAccounts++] = make;
-//		System.out.println("계좌갯수: " + numOfAccounts);
-//		
-//		System.out.println(iAcnumber);
-//		System.out.println(iName);
-//		System.out.println(iMoney);
-//		System.out.println(acnumber);
-//		
-//		System.out.println(make.acnumber);
-//		System.out.println(make.name);
-//		System.out.println(make.mymoney);
-//		
-//	}
-	public void makeAccount() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("***신규계좌개설***");
-		System.out.print("계좌번호:");acnumber = scanner.nextLine();
-		System.out.print("고객이름:");name = scanner.nextLine();
-		System.out.print("잔고:");mymoney = scanner.nextInt();
-		System.out.println("계좌개설이 완료되었습니다.");
-		System.out.println("계좌갯수: " + numOfAccounts);
-		
-		System.out.println(acnumber);
-		System.out.println(name);
-		System.out.println(mymoney);
-//		myAccounts[numOfAccounts++];
-//		a
-		
-		
-	}
-	
-	public void depositMoney() {
-		Scanner scanner = new Scanner(System.in);
-		String iAcnumber;
-		int iMoney;
-		System.out.println("***입 금***");
-		System.out.println("계좌번호와 입금할 금액을 입력하세요");
-		System.out.print("계좌번호:");iAcnumber = scanner.nextLine();
-		System.out.print("입금액:");iMoney = scanner.nextInt();
-		System.out.println("입금이 완료되었습니다.");
-//		if(iAcnumber.compareTo(ma[acnumber])==0) {
-			mymoney += iMoney;
-//			System.out.println("일치합니다");
-//			System.out.println(acnumber);
-//			System.out.println(name);
-//			System.out.println(mymoney);
-//		} else {
-//			System.out.println("불일치합니다");
-//			System.out.println(acnumber);
-//			System.out.println(name);
-//			System.out.println(mymoney);
-//		}
-	}
-//	public void withdrawMoney() {
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.println("***출 금***");
-//		System.out.println("계좌번호와 출금할 금액을 입력하세요");
-//		System.out.print("계좌번호:");numcode = scanner.nextLine();
-//		System.out.print("출금액:");money = scanner.nextInt();
-//		if(acnumber == numcode  && mymoney >= money) {
-//			mymoney -= money;
-//			System.out.println("일치합니다");
-//			System.out.println("출금이 완료되었습니다.");
-//		} else {
-//			System.out.println("잔고가 부족합니다.");
-//		}
-//		
-//	}
-	public void showAccInfo() {
-		for(int i=0 ; i<numOfAccounts ; i++) {
-			myAccounts[i].showAccInfo();
-		}
+
+	public void showAllData() {
 		System.out.println("계좌번호:" + acnumber);
 		System.out.println("고객이름:" + name);
 		System.out.println("잔고:" + mymoney);
-		System.out.println("전체계좌정보 출력이 완료되었습니다.");
 	}
-	
-	
-	
-	
-
 }
--- src/banking1/JavaConsoleBankPSH.java
+++ src/banking1/JavaConsoleBankPSH.java
@@ -4,36 +4,114 @@ import java.util.Scanner;
 
 public class JavaConsoleBankPSH {
 
-	public static void main(String[] args) {
-		
+	static Account[] myAccounts = new Account[50];
+	static int numOfAccounts;
+
+	static void menuShow() {
+		System.out.println("----------Menu---------");
+		System.out.print("1.계좌 개설 ");
+		System.out.print("2.입 금 ");
+		System.out.println("3.출 금 ");
+		System.out.print("4.계좌정보출력 ");
+		System.out.print("5.프로그램종료 ");
+	}
+
+	static void makeAccount() {
 		Scanner scanner = new Scanner(System.in);
-		Account account = new Account();
+		String iAcnumber;
+		String iName;
+		int iMoney;
+		System.out.println("***신규계좌개설***");
+		System.out.print("계좌번호:");
+		iAcnumber = scanner.nextLine();
+		System.out.print("고객이름:");
+		iName = scanner.nextLine();
+		System.out.print("잔고:");
+		iMoney = scanner.nextInt();
+		System.out.println("계좌개설이 완료되었습니다.");
+		Account make = new Account(iAcnumber, iName, iMoney);
+		myAccounts[numOfAccounts++] = make;
+
+	}
+
+	static void depositMoney() {
+		Scanner scanner = new Scanner(System.in);
+		String iAcnumber;
+		int money;
+		System.out.println("***입 금***");
+		System.out.println("계좌번호와 입금할 금액을 입력하세요");
+		System.out.print("계좌번호 : ");
+		iAcnumber = scanner.nextLine();
+		System.out.print("입금액 : ");
+		money = scanner.nextInt();
+		for (int i=0 ; i<numOfAccounts ; i++) {
+			if(iAcnumber.compareTo(myAccounts[i].acnumber)==0) {
+				myAccounts[i].mymoney += money;
+				System.out.println("입금이 완료되었습니다.");
+			} 
+		}
+	}
+
+	static void withdrawMoney() {
+		Scanner scanner = new Scanner(System.in);
+		String iAcnumber;
+		int money;
+		System.out.println("***출 금***");
+		System.out.println("계좌번호와 출금할 금액을 입력하세요");
+		System.out.print("계좌번호 : ");
+		iAcnumber = scanner.nextLine();
+		System.out.print("출금액 : ");
+		money = scanner.nextInt();
+		for (int i=0 ; i<numOfAccounts ; i++) {
+			if(iAcnumber.compareTo(myAccounts[i].acnumber)==0) {
+				if(myAccounts[i].mymoney >= money) {
+					myAccounts[i].mymoney -= money;
+					System.out.println("출금이 완료되었습니다.");
+				} else {
+					System.out.println("출금이 거절되었습니다.");
+				}
+			}
+		}
+	}
+	
+	static void showAccInfo() {
+		for (int i = 0; i < numOfAccounts; i++) {
+			System.out.println("계좌번호 : " + myAccounts[i].acnumber);
+			System.out.println("고객명 : " + myAccounts[i].name);
+			System.out.println("잔고 : " + myAccounts[i].mymoney);
+			System.out.println("--------------");
+			System.out.println("전체계좌정보 출력이 완료되었습니다.");
+		}
+
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
 		
-		
-		while(true) {
-			account.menuShow();
+		while (true) {
+			menuShow();
 			int choice = scanner.nextInt();
-			switch(choice) {
-			case BankMenu.MAKE:				
-				account.makeAccount();
+			switch (choice) {
+			case BankMenu.MAKE:
+				makeAccount();
 				break;
 			case BankMenu.DEPOSIT:
-				account.depositMoney();
+				depositMoney();
 				break;
-//			case BankMenu.WITHDRAW:
-//				account.withdrawMoney();
-//				break;
+			case BankMenu.WITHDRAW:
+				withdrawMoney();
+				break;
 			case BankMenu.INQUIRE:
-				account.showAccInfo();
+				showAccInfo();
 				break;
 			case BankMenu.EXIT:
 				System.out.println("프로그램을 종료합니다");
 				return;
-				
+
 			}
 		}
-		
-		
 
 	}
 
